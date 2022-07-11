e182b5ad4e624c5a8d2d0967ca0f55393b74c433
MateoCb99 <cuellobuiles@gmail.com>
2022-07-11 13:07:38 -0500

    FINALIZADO

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -198,7 +198,8 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  return +nombre+''+apellido;
+  var combinado = nombre +' '+apellido;
+  return combinado;
   
 }
 
@@ -206,7 +207,8 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return 'hola'+nombre+'!';
+  saludo = "Hola"+ " "+nombre+"!"
+  return saludo;
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
@@ -248,13 +250,9 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if(letra.length>1){
-    return "Dato incorrecto"
-  }
-  if(letra===a||letra===e||letra===i||letra===o||latra===u) {
-    
-  }
-  return "Es una vocal"
+  if(letra==="a"||letra==="e"||letra==="i"||letra==="o"||letra==="u") return "Es vocal"
+  if(letra.length<2)return "Dato incorrecto"
+  return "Dato incorrecto"
 }
 
 
